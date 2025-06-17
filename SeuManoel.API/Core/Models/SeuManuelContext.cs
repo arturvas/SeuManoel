502dd97dc4c7cf9b96df106761a79527d5c3b052
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2025-06-17 14:00:51 -0400

    feat: add `Pedidos` and `Produtos` DbSets in `SeuManoelContext`

--- SeuManoel.API/Core/Models/SeuManuelContext.cs
+++ SeuManoel.API/Core/Models/SeuManuelContext.cs
@@ -5,7 +5,9 @@ namespace SeuManoel.API.Core.Models
     public class SeuManoelContext : DbContext
     {
         public SeuManoelContext(DbContextOptions<SeuManoelContext> options)
-            : base(options)
-        { }
+            : base(options) { }
+        
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
     }
 }
